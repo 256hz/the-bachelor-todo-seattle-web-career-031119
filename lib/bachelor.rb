7982f7fb5d61e8e23f7714b7eda548af44919000
Abe D <iaryiou111@gmail.com>
2019-03-06 21:02:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
-    binding.pry
+    #binding.pry
     if data[season][contestant]["status"] == "Winner"
       return data[season][contestant]["name"].split(" ")[0]
     end
