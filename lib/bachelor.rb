e57af3c3e8b18361cf522c4e800510f88a6df578
Abe D <iaryiou111@gmail.com>
2019-03-06 21:08:42 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,16 +3,19 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   data[season].each.with_index do |block, index|
     person = data[season][index]
-    #binding.pry
     if person["status"] == "Winner"
-      #binding.pry
       return person["name"].split(" ")[0]
     end
   end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data[season].each.with_index do |block, index|
+    person = data[season][index]
+    if person["occupation"] == occupation
+      return person["name"]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
